1712f900e4d508198b87bc9463b11ba71547046b
el1s5a <valleselissa0@gmail.com>
2023-10-24 13:30:20 -0700

    Finished simple_functions exercise

---
+++ simple_functions.js
@@ -0,0 +1,16 @@
+function favoriteThings()
+{
+    console.log("Elissa")
+    console.log("The Batman")
+    console.log("The Garden")
+}
+
+function whyImHere()
+{
+    console.log("To Learn More")
+}
+
+function favoritePlace()
+{
+    console.log("My favorite place to visit is Sedona,Arizona because of the nice weather they have and the calming creeks.")
+}
